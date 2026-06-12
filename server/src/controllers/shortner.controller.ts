1889d8d0623a495a0db4bba7ef447a7ea92d375c
Satish Prajapati <80681460+Satish9047@users.noreply.github.com>
2026-06-12 08:39:57 +0545

    update the controller paths

--- server/src/controllers/shortner.controller.ts
+++ server/src/controllers/shortner.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
-import {asyncHandler} from "../utils/asyncHandler.utils";
+// import {asyncHandler} from "../utils/asyncHandler.utils";
 import { ApiResponse } from "../utils/apiResponse.utils";
+import * as urlShortnerService from "../services/shortner.service";
 
-export const shortUrlHandler = (req: Request, res: Response) => {
-    const { url } = req.body;
-    const data = await recipeService.addPizzaBaseService(url);
-    res.json(new ApiResponse(200, "Pizza base added successfully", data));
+export const shortUrlHandler = async (req: Request, res: Response) => {
+    const { url }: { url: string } = req.body;
+    const data = await urlShortnerService.shortenUrl(url);
+    res.json(new ApiResponse(200, "URL shortened successfully", data));
 }
 
 // export const shortUrlHandler = asyncHandler(async (req: Request, res: Response) => {
--- server/src/interfaces/index.ts
+++ server/src/interfaces/index.ts
@@ -2,4 +2,5 @@ export interface AppConfig {
     PORT: number | string;
     ENV: string;
     DATABASE_URL: string;
+    BASE_SHORT_URL: string;
 }

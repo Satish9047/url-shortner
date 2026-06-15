49b46d30f8cd3b94908419446eff0ab62f84127c
Satish Prajapati <80681460+Satish9047@users.noreply.github.com>
2026-06-15 07:38:04 +0545

    update:  url validation in zod schema

--- server/src/schema/url.schema.ts
+++ server/src/schema/url.schema.ts
@@ -2,5 +2,6 @@ import { z } from "zod";
 
 export const urlSchema = z.object({
   url: z
-    .string({ message: "Invalid URL" })
+    .url({ message: "Invalid URL. Please enter a valid URL" })
+    .min(1, { message: "URL cannot be empty" }),
 });

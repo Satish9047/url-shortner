f4975e64e3bc4c8c0597453fd001338dfbb8f909
Satish Prajapati <80681460+Satish9047@users.noreply.github.com>
2026-06-15 11:44:57 +0545

    use prettier for formatting the code

--- server/src/controllers/shortner.controller.ts
+++ server/src/controllers/shortner.controller.ts
@@ -1,31 +1,37 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { ApiResponse, ApiError } from "../utils/apiResponse.utils";
 import * as urlShortnerService from "../services/shortner.service";
 
 export const shortUrlHandler = async (req: Request, res: Response) => {
-    const { url }: { url: string } = req.body;
-    const data = await urlShortnerService.shortenUrl(url);
-    res.json(new ApiResponse(200, "URL shortened successfully", data));
-}
+  const { url }: { url: string } = req.body;
+  const data = await urlShortnerService.shortenUrl(url);
+  res.json(new ApiResponse(200, "URL shortened successfully", data));
+};
 
 export const listUrlsHandler = async (req: Request, res: Response) => {
-    const data = await urlShortnerService.getAllUrls();
-    res.json(new ApiResponse(200, "Successfully Listed all the URLs", data));
-}
+  const data = await urlShortnerService.getAllUrls();
+  res.json(new ApiResponse(200, "Successfully Listed all the URLs", data));
+};
 
 export const analyticsHandler = async (req: Request, res: Response) => {
-    const { code } = req.params;
-    console.log("analytics", code);
-    const data = await urlShortnerService.getUrlAnalytics(code);
-    console.log(data);
-    res.json(new ApiResponse(200, "Successfully fetch URLs click analytics data", data));
-}
+  const { code } = req.params;
+  console.log("analytics", code);
+  const data = await urlShortnerService.getUrlAnalytics(code);
+  console.log(data);
+  res.json(
+    new ApiResponse(200, "Successfully fetch URLs click analytics data", data),
+  );
+};
 
-export const redirectHandler = async (req: Request, res: Response) => {
-    const { code } = req.params;
-    const data = await urlShortnerService.getOriginalURL(code);
-    if(data){
-        res.redirect(302, data);
-    }
-    res.status(404).json(new ApiError(404, "Short URL not found"));
-}
+export const redirectHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { code } = req.params;
+  const data = await urlShortnerService.getOriginalURL(code);
+  if (data) {
+    return res.redirect(302, data);
+  }
+  return next(new ApiError(404, "Short URL not found"));
+};
